8700be72c02eaf2bef8ba46fff0351c49e5862ef
Ayumu AIZAWA <ayumu.aizawa@gmail.com>
2017-07-02 21:40:53 +0900

    fixed typo

--- main.rb
+++ main.rb
@@ -28,7 +28,7 @@ class MainApp < Sinatra::Base
     Twilio::TwiML::Response.new do |r|
       conn = Faraday.new(:url => @rootPath) do |faraday|
         faraday.request :url_encoded
-        faraday.respose :logger
+        faraday.response :logger
         faraday.adapter Faraday.default_adapter
       end
       conn.post "/sendsms/#{fromNumber}"
